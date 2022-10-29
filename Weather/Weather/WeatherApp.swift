cafedecf85ceb078f52ae017c12d48ae71eaefd4
Vladimir Fibe <fibe@Vladimirs-MacBook-Air.local>
2022-10-29 16:33:31 +0600

    Add Weather App

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Vladimir Fibe on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
